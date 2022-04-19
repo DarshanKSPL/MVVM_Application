c727f14bd68736605cc45b3c800766a65f10011b
DarshanKSPL <darshan.rathod@kanerika.com>
2022-04-19 23:22:46 +0530

    Show greeting text

--- app/src/main/java/com/example/mvvmapplication/MainActivity.kt
+++ app/src/main/java/com/example/mvvmapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.mvvmapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.mvvmapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var  binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.btnShow.setOnClickListener {
+            Toast.makeText(this,"Good morning..!!, ${binding.etName.text}",Toast.LENGTH_SHORT)
+        }
     }
 }
